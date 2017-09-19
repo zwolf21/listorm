16343a71f73e999a67364ca480a68bbeaee9fa9f
imac <imac@imacui-iMac.local>
2017-09-19 21:55:29 +0900

    filter_splited

--- listorm.py
+++ listorm.py
@@ -4,7 +4,7 @@ from heapq import nlargest, nsmallest
 from io import BytesIO, StringIO
 from collections import namedtuple, Counter, OrderedDict, defaultdict
 from copy import deepcopy
-import csv
+import csv, re
 
 import xlrd, xlsxwriter
 
@@ -414,6 +414,48 @@ class Listorm(list):
 
         Changes = namedtuple('Changes', 'added deleted updated')
         return Changes(added, deleted, updated)
+
+    def filter_splited(self,  keyword, *columns, splitby=['space', 'nospace', 'digit'], exclude=False):
+        '''splitby = ['space', 'nospace', 'digit']
+        '''
+
+        for sep in splitby:
+
+            lst = Listorm(self)
+            
+            if exclude:
+                excluded = Listorm(self)
+
+            ismatch = lambda keyword, text: re.search(keyword, text)
+
+            if sep == 'space':
+                tokkens = re.split('\s+', keyword)
+            elif sep == 'nospace':
+                ismatch = lambda keyword, text: re.search(re.sub('\s+','', keyword), re.sub('\s+','', text))
+                tokkens = [keyword]
+            elif sep == 'digit':
+                tokkens = re.split('\d+', keyword) + re.findall('\d+', keyword)     
+
+            for tok in tokkens:
+                filtered = Listorm()
+                for col in columns:
+                    filtered += lst.filter(where=lambda row: ismatch(tok, row[col]))
+                if filtered:
+                    lst = filtered
+
+            if lst:
+                if len(lst) == len(self):
+                    continue
+
+                if exclude:
+                    for col in columns:
+                        colvalues = lst.unique(col)
+                        excluded = excluded.filter(where=lambda row: row[col] not in colvalues)
+                    return excluded
+                else:
+                    return lst
+
+        return Listorm()
 
 
 
