c45f789f20c5eab82350958a127af6e252e9da45
HS Moon <pbr112@naver.com>
2021-08-12 03:36:10 +0900

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='listorm',
-	version='0.2.15',
+	version='0.2.16',
 	description='list API for table type of dict-list',
 	author = 'HS Moon',
 	author_email = 'mhs9089@gmail.com',
