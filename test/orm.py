23214a3f593ba0dc6386cbe3d3743fea0656d14a
zwolf21 <pbr112@naver.com>
2022-06-30 12:35:49 +0900

    add testes

---
+++ test/orm.py
@@ -0,0 +1,128 @@
+import pytest
+
+from listorm import Listorm
+from listorm.exceptions import UniqueConstraintError
+from .samples import *
+
+
+
+test_init_arg_cases = [
+    (userTable, None, False, None, userTable),
+    (userTable_missing_values, None, False, None, userTable_missing_values),
+    (userTable_missing_values, None, False, None, userTable_missing_values),
+    (userTable_missing_values, None, True, 'undefined', userTable_missing_values_filling_with_undefined),
+    (userTable, ('location', 'gender',), False, None, userTable),
+    (userTable_missing_values, 'name', False, None, userTable_missing_values),
+]
+@pytest.mark.parametrize('records, uniques, fill_missed, fill_value, results', test_init_arg_cases)
+def test_init(records, uniques, fill_missed, fill_value, results):
+    if not uniques:
+        lst = Listorm(records,uniques,fill_missed,fill_value)
+        assert lst == results
+
+    if uniques:
+        with pytest.raises(UniqueConstraintError):
+            lst = Listorm(records,uniques,fill_missed,fill_value)
+
+
+test_select_cases = [
+    (userTable, 'name', ['location'], None, userTable_select_name_exclude_location),
+    (userTable, [], None, lambda row: row.age > 20, userTable_age_over_20),
+    (userTable, ['name', 'gender', 'location' ], None, lambda row: row.age > 20, userTable_select_name_gender_location_where_age_gt_20),
+]
+@pytest.mark.parametrize('records, columns, excludes, where, results', test_select_cases)
+def test_select(records, columns, excludes, where, results):
+    lst = Listorm(records).select(columns, excludes=excludes, where=where)
+    assert lst == results
+
+
+test_drop_column_cases = [
+    (userTable, ['age'], userTable_dropped_age),
+    (userTable, ['gender', 'location'], userTable_dropped_gender_location),
+]
+@pytest.mark.parametrize('records, columns, results', test_drop_column_cases)
+def test_drop_column(records, columns, results):
+    lst = Listorm(records).drop_column(columns)
+    assert lst == results
+
+
+test_add_column_cases = [
+    (userTable, {'key': 'value'}, userTable_column_added_value),
+    (userTable, {'gender/age': lambda row: '{}/{}'.format(row.gender, row.age)}, userTable_column_added_gender_age_concated)
+]
+@pytest.mark.parametrize('records, column_mapset, results', test_add_column_cases)
+def test_add_column(records, column_mapset, results):
+    lst = Listorm(records).add_column(column_mapset)
+    assert lst == results
+
+
+test_rename_cases = [
+    (userTable, {'location': 'address'}, userTable_renamed_location2address),
+    (userTable, {'location': 'address', 'gender': 'sex'}, userTable_renamed_location2address_gender2sex),
+]
+@pytest.mark.parametrize('records, renamemap, results', test_rename_cases)
+def test_rename(records, renamemap, results):
+    lst = Listorm(records).rename(renamemap)
+    assert lst == results
+
+
+test_update_cases = [
+    (userTable, {'name': str.upper}, None, userTable_updated_name_upper),
+    (userTable, {'name': str.upper}, lambda row: row.age > 20, userTable_updated_name_upper_where_age_gt_20),
+    (userTable, {'gender': lambda value, row: "{}/{}".format(value, row.age) }, None, userTable_updated_gender_concat_with_age),
+]
+@pytest.mark.parametrize('records, renamemap, where, results', test_update_cases)
+def test_update(records, renamemap, where, results):
+    lst = Listorm(records).update(renamemap, where=where)
+    assert lst == results
+
+
+test_values_cases = [
+    (userTable, ['name'], True, userTable_values_by_name),
+    (userTable, ['name'], False, userTable_values_by_name_flat_flase),
+    (userTable, ['name', 'gender'], True, userTable_values_by_name_and_gender),
+]
+@pytest.mark.parametrize('records, columns, flat_one, results', test_values_cases)
+def test_values(records, columns, flat_one, results):
+    lst = Listorm(records).values(columns, flat_one=flat_one)
+    assert lst == results
+
+
+test_distinct_cases = [
+    (buyTable, ['name'], True, False, buyTable_distinct_by_name_keep_first),
+    (buyTable, ['name'], False, False, buyTable_distinct_by_name_keep_last),
+    (buyTable, ['name'], True, True, buyTable_distinct_by_name_singles),
+]
+@pytest.mark.parametrize('records, columns, first, singles, results', test_distinct_cases)
+def test_distinct(records, columns, first, singles, results):
+    assert results == Listorm(records).distinct(columns, first=first, singles=singles)
+
+
+test_orderby_cases = [
+    (userTable, ['location', '-age'], userTable_orderby_location_age_desc)
+]
+@pytest.mark.parametrize('records, sortkeys, results', test_orderby_cases)
+def test_orderby(records, sortkeys, results):
+    assert results == Listorm(records).orderby(sortkeys)
+
+test_groupby_cases = [
+    (userTable, ['location', 'gender'], {'gender': len, 'age': lambda vals: sum(vals)/len(vals)}, {'gender': 'count', 'age': 'avg_age'}, userTable_groupby_location_gender)
+]
+@pytest.mark.parametrize('records, columns, aggset, renames, results', test_groupby_cases)
+def test_groupby(records, columns, aggset, renames, results):
+    assert results == Listorm(records).groupby(columns, aggset=aggset, renames=renames)
+
+
+test_join_cases = [
+    (userTable, buyTable, 'name', None, 'inner', user_buy_table_inner_joined),
+    (userTable, buyTable, 'name', None, 'left', user_buy_table_left_joined),
+    (userTable, buyTable, 'name', None, 'right', user_buy_table_right_joined),
+    (userTable, buyTable, 'name', None, 'outer', user_buy_table_outer_joined),
+]
+@pytest.mark.parametrize('records, other, on, right_on, how, results', test_join_cases)
+def test_join(records, other, on, right_on, how, results):
+    assert results == Listorm(records, fill_value= '') \
+        .join(other, on, right_on, how) \
+        .orderby('name', 'product') \
+        .set_number_type(amount=0.0)
+
---
+++ test/samples/results.py
@@ -0,0 +1,263 @@
+userTable_missing_values_filling_with_undefined = [
+    {'age': 18, 'location': 'Korea', 'name': 'undefined', 'gender': 'undefined'},
+    {'name': 'Charse', 'gender': 'M', 'location': 'USA', 'age': 'undefined'},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'undefined', 'age': 'undefined', 'location': 'undefined'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA'},
+    {'name': 'undefined', 'gender': 'undefined', 'age': 'undefined', 'location': 'Korea'},
+]
+
+userTable_select_name_exclude_location = [
+    {'name': 'Hong'},
+    {'name': 'Charse'},
+    {'name': 'Lyn'},
+    {'name': 'Xiaomi'},
+    {'name': 'Park'},
+    {'name': 'Smith'},
+    {'name': 'Lee'},
+]
+
+userTable_age_over_20 = [
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+]
+
+userTable_select_name_gender_location_where_age_gt_20 = [
+    {'name': 'Lyn', 'gender': 'F', 'location': 'China'},
+    {'name': 'Park', 'gender': 'M', 'location': 'Korea'},
+]
+
+userTable_dropped_age = [
+    {'name': 'Hong', 'gender': 'M', 'location': 'Korea'},
+    {'name': 'Charse', 'gender': 'M', 'location': 'USA'},
+    {'name': 'Lyn', 'gender': 'F', 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M', 'location': 'China'},
+    {'name': 'Park', 'gender': 'M', 'location': 'Korea'},
+    {'name': 'Smith', 'gender': 'M', 'location': 'USA'},
+    {'name': 'Lee', 'gender': 'F', 'location': 'Korea'},
+]
+
+userTable_dropped_gender_location = [
+    {'name': 'Hong', 'age': 18},
+    {'name': 'Charse', 'age': 19},
+    {'name': 'Lyn', 'age': 28},
+    {'name': 'Xiaomi', 'age': 15},
+    {'name': 'Park', 'age': 29},
+    {'name': 'Smith', 'age': 17},
+    {'name': 'Lee', 'age': 12},
+]
+
+userTable_column_added_gender_age_concated = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'gender/age': 'M/18'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA', 'gender/age': 'M/19'},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'gender/age': 'F/28'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'gender/age': 'M/15'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'gender/age': 'M/29'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'gender/age': 'M/17'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'gender/age': 'F/12'},
+]
+
+userTable_column_added_value = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'key': 'value'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA', 'key': 'value'},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'key': 'value'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'key': 'value'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'key': 'value'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'key': 'value'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'key': 'value'},
+]
+
+userTable_renamed_location2address = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'address': 'Korea'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'address': 'USA'},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'address': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'address': 'China'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'address': 'Korea'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'address': 'USA'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'address': 'Korea'},
+]
+userTable_renamed_location2address_gender2sex = [
+    {'name': 'Hong', 'sex': 'M', 'age': 18, 'address': 'Korea'},
+    {'name': 'Charse', 'sex': 'M', 'age': 19, 'address': 'USA'},
+    {'name': 'Lyn', 'sex': 'F', 'age': 28, 'address': 'China'},
+    {'name': 'Xiaomi', 'sex': 'M', 'age': 15, 'address': 'China'},
+    {'name': 'Park', 'sex': 'M', 'age': 29, 'address': 'Korea'},
+    {'name': 'Smith', 'sex': 'M', 'age': 17, 'address': 'USA'},
+    {'name': 'Lee', 'sex': 'F', 'age': 12, 'address': 'Korea'},
+]
+
+userTable_updated_name_upper = [
+    {'name': 'HONG', 'gender': 'M', 'age': 18, 'location': 'Korea'},
+    {'name': 'CHARSE', 'gender': 'M', 'age': 19, 'location': 'USA'},
+    {'name': 'LYN', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'XIAOMI', 'gender': 'M', 'age': 15, 'location': 'China'},
+    {'name': 'PARK', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+    {'name': 'SMITH', 'gender': 'M', 'age': 17, 'location': 'USA'},
+    {'name': 'LEE', 'gender': 'F', 'age': 12, 'location': 'Korea'},
+]
+userTable_updated_name_upper_where_age_gt_20 = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA'},
+    {'name': 'LYN', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China'},
+    {'name': 'PARK', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea'},
+]
+userTable_updated_gender_concat_with_age = [
+    {'name': 'Hong', 'gender': 'M/18', 'age': 18, 'location': 'Korea'},
+    {'name': 'Charse', 'gender': 'M/19', 'age': 19, 'location': 'USA'},
+    {'name': 'Lyn', 'gender': 'F/28', 'age': 28, 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M/15', 'age': 15, 'location': 'China'},
+    {'name': 'Park', 'gender': 'M/29', 'age': 29, 'location': 'Korea'},
+    {'name': 'Smith', 'gender': 'M/17', 'age': 17, 'location': 'USA'},
+    {'name': 'Lee', 'gender': 'F/12', 'age': 12, 'location': 'Korea'},
+]
+
+userTable_values_by_name = ['Hong','Charse','Lyn','Xiaomi','Park','Smith','Lee']
+userTable_values_by_name = ['Hong','Charse','Lyn','Xiaomi','Park','Smith','Lee']
+userTable_values_by_name_flat_flase = [('Hong',),('Charse',),('Lyn',),('Xiaomi',),('Park',),('Smith',),('Lee',)]
+userTable_values_by_name_and_gender = [
+    ("Hong", "M",),
+    ("Charse", "M",),
+    ("Lyn", "F",),
+    ("Xiaomi", "M",),
+    ("Park", "M",),
+    ("Smith", "M",),
+    ("Lee", "F",),
+]
+
+buyTable_distinct_by_name_keep_first = [
+    {'name': 'Xiaomi', 'product': 'battery', 'amount':7},
+    {'name': 'Hong', 'product': 'keyboard', 'amount':1},
+    {'name': 'Lyn', 'product': 'cleaner', 'amount':5},
+    {'name': 'Unknown', 'product': 'keyboard', 'amount':1},
+    {'name': 'Lee', 'product': 'hardcase', 'amount':2},
+    {'name': 'Yuki', 'product': 'manual', 'amount':1},
+    {'name': 'anonymous', 'product': 'adopter', 'amount':2},
+    {'name': 'Park', 'product': 'battery', 'amount':2},
+    {'name': 'Smith', 'product': 'mouse', 'amount':1},
+]
+buyTable_distinct_by_name_keep_last = [
+    {'name': 'Lyn', 'product': 'mouse', 'amount':1},
+    {'name': 'Unknown', 'product': 'keyboard', 'amount':1},
+    {'name': 'Lee', 'product': 'keycover', 'amount':2},
+    {'name': 'Yuki', 'product': 'manual', 'amount':1},
+    {'name': 'Xiaomi', 'product': 'cable', 'amount':1},
+    {'name': 'anonymous', 'product': 'adopter', 'amount':2},
+    {'name': 'Park', 'product': 'battery', 'amount':2},
+    {'name': 'Hong', 'product': 'cleaner', 'amount':3},
+    {'name': 'Smith', 'product': 'mouse', 'amount':1},
+]
+buyTable_distinct_by_name_singles = [
+    {'name': 'Unknown', 'product': 'keyboard', 'amount':1},
+    {'name': 'Yuki', 'product': 'manual', 'amount':1},
+    {'name': 'anonymous', 'product': 'adopter', 'amount':2},
+    {'name': 'Park', 'product': 'battery', 'amount':2},
+    {'name': 'Smith', 'product': 'mouse', 'amount':1},
+]
+
+
+userTable_orderby_location_age_desc = [
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA'},
+]
+
+userTable_groupby_location_gender = [
+    {'location': 'Korea', 'gender': 'M', 'count': 2, 'avg_age': 23.5},
+    {'location': 'USA', 'gender': 'M', 'count': 2, 'avg_age': 18.0},
+    {'location': 'China', 'gender': 'F', 'count': 1, 'avg_age': 28.0},
+    {'location': 'China', 'gender': 'M', 'count': 1, 'avg_age': 15.0},
+    {'location': 'Korea', 'gender': 'F', 'count': 1, 'avg_age': 12.0}
+]
+
+
+user_buy_table_inner_joined = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'cleaner', 'amount': 3},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'keyboard', 'amount': 1},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'monitor', 'amount': 1},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'mouse', 'amount': 3},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'hardcase', 'amount': 2},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'keycover', 'amount': 2},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'cleaner', 'amount': 5},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'mouse', 'amount': 1},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'product': 'battery', 'amount': 2},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'product': 'mouse', 'amount': 1},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'battery', 'amount': 7},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'cable', 'amount': 1},
+]
+
+user_buy_table_left_joined = [
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA', 'product': '', 'amount': 0.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'cleaner', 'amount': 3.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'keyboard', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'monitor', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'mouse', 'amount': 3.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'hardcase', 'amount': 2.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'keycover', 'amount': 2.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'cleaner', 'amount': 5.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'product': 'battery', 'amount': 2.0},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'battery', 'amount': 7.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'cable', 'amount': 1.0},
+]
+
+user_buy_table_right_joined = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'cleaner', 'amount': 3.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'keyboard', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'monitor', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'mouse', 'amount': 3.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'hardcase', 'amount': 2.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'keycover', 'amount': 2.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'cleaner', 'amount': 5.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'product': 'battery', 'amount': 2.0},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Unknown', 'gender': '', 'age': '', 'location': '', 'product': 'keyboard', 'amount': 1.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'battery', 'amount': 7.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'cable', 'amount': 1.0},
+    {'name': 'Yuki', 'gender': '', 'age': '', 'location': '', 'product': 'manual', 'amount': 1.0},
+    {'name': 'anonymous', 'gender': '', 'age': '', 'location': '', 'product': 'adopter', 'amount': 2.0},
+]
+
+user_buy_table_outer_joined = [
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA', 'product': '', 'amount': 0.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'cleaner', 'amount': 3.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'keyboard', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'monitor', 'amount': 1.0},
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea', 'product': 'mouse', 'amount': 3.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'hardcase', 'amount': 2.0},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea', 'product': 'keycover', 'amount': 2.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'cleaner', 'amount': 5.0},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea', 'product': 'battery', 'amount': 2.0},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA', 'product': 'mouse', 'amount': 1.0},
+    {'name': 'Unknown', 'gender': '', 'age': '', 'location': '', 'product': 'keyboard', 'amount': 1.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'battery', 'amount': 7.0},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China', 'product': 'cable', 'amount': 1.0},
+    {'name': 'Yuki', 'gender': '', 'age': '', 'location': '', 'product': 'manual', 'amount': 1.0},
+    {'name': 'anonymous', 'gender': '', 'age': '', 'location': '', 'product': 'adopter', 'amount': 2.0},
+]
+
+
+userTable = [
+    {'name': 'Hong', 'gender': 'M', 'age': 18, 'location': 'Korea'},
+    {'name': 'Charse', 'gender': 'M', 'age': 19, 'location': 'USA'},
+    {'name': 'Lyn', 'gender': 'F', 'age': 28, 'location': 'China'},
+    {'name': 'Xiaomi', 'gender': 'M', 'age': 15, 'location': 'China'},
+    {'name': 'Park', 'gender': 'M', 'age': 29, 'location': 'Korea'},
+    {'name': 'Smith', 'gender': 'M', 'age': 17, 'location': 'USA'},
+    {'name': 'Lee', 'gender': 'F', 'age': 12, 'location': 'Korea'},
+]
+
+
+
+
+
