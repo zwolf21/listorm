4b3d22f50fb99e18645f140ee208ab41f289848a
zwolf21 <pbr112@naver.com>
2022-06-30 12:35:18 +0900

    setup.py settings

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+	name='listorm',
+	version='1.0.0',
+	license='MIT',
+	description='smart and convenient dict flatten library for complex container nested with dict and list',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author = 'HS Moon',
+	author_email = 'pbr112@naver.com',
+	py_modules = ['listorm'],
+	install_requires=[],
+	keywords=['listorm', 'list of dict'],
+	url='https://github.com/zwolf21/listorm',
+	packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+	classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+	python_requires=">=3.6"
+)
